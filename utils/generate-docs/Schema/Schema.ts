0a59a5ca6cf7e5104810391ffae7ec267ad9cfdf
John Scrudato <jscrudato@gunder.com>
2022-12-13 10:42:14 -0600

    Fixed broken links in Architecture.md and also turned off TOC generation.

--- utils/generate-docs/Schema/Schema.ts
+++ utils/generate-docs/Schema/Schema.ts
@@ -42,20 +42,7 @@ export default class Schema {
     );
     await SchemaWriter.write(REPO_ROOT, schema);
 
-    // Suggest we don't generate our own TOC anymore as MkDocs is doing it.
-    // await TableOfContents.write(
-    //   schema,
-    //   REPO_ROOT,
-    //   path.join("docs", "README.md")
-    // );
-
-    // Suggest we autogenerate a TOC for each dir in schema_markdown/schema so we can link to "Folders"
-    // still which will provide an index to the relevant concepts in that folder.
-    await TableOfContents.write_index(
-      schema,
-      REPO_ROOT,
-      path.join("docs", "schema_markdown", "INDEX.md")
-    );
+    // Note - switched off generating TOCs per TWG discussion
   };
 
   readonly schemaNodes: SchemaNode[];
